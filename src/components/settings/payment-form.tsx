77f5644ca6899e083d45f26df2256a6525850e77
RaulLoyola <raul.loy@gmail.com>
2024-07-21 00:37:07 -0600

    fixing payment form

--- src/components/settings/payment-form.tsx
+++ src/components/settings/payment-form.tsx
@@ -1,31 +1,27 @@
-'use client'
-import React from 'react'
-import { CardDescription } from '../ui/card'
-import { Loader } from '../loader'
-import { PaymentElement } from '@stripe/react-stripe-js'
-import { Button } from '../ui/button'
-import { useCompletePayment } from '@/hooks/billing/use-billing'
-
+'use client';
+import React from 'react';
+import { CardDescription } from '../ui/card';
+import { Loader } from '../loader';
+// import { PaymentElement } from '@stripe/react-stripe-js'
+import { Button } from '../ui/button';
+// import { useCompletePayment } from '@/hooks/billing/use-billing'
 
 type PaymentFormProps = {
-  plan: 'STANDARD' | 'PRO' | 'ULTIMATE'
-}
+  plan: 'STANDARD' | 'PRO' | 'ULTIMATE';
+};
 
 export const PaymentForm = ({ plan }: PaymentFormProps) => {
-  const { processing, onMakePayment } = useCompletePayment(plan)
+  // const { processing, onMakePayment } = useCompletePayment(plan)
   return (
-    <form
-      onSubmit={onMakePayment}
-      className="flex flex-col gap-5"
-    >
+    <form onSubmit={() => {}} className="flex flex-col gap-5">
       <div>
         <h2 className="font-semibold text-xl text-black">Payment Method</h2>
         <CardDescription>Enter your card details</CardDescription>
       </div>
-      <PaymentElement />
+      {/* <PaymentElement />
       <Button type="submit">
         <Loader loading={processing}>Pay</Loader>
-      </Button>
+      </Button> */}
     </form>
-  )
-}
+  );
+};
